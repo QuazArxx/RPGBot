cb39265afb5bc8d43b6b5145c2241dc95b2f6ed1
QuazArxx <33949974+QuazArxx@users.noreply.github.com>
2021-02-25 15:43:49 -0500

    Finished command, potential future modifications

--- commands/open.js
+++ commands/open.js
@@ -1,8 +1,10 @@
+const Discord = require('discord.js');
 const fs = require('fs');
 
 const functions = require('../functions.js');
 const messages = require('../messages.js');
 const players = require('../players.json');
+const colors = require('../colors.json');
 
 module.exports = {
     name: 'open',
@@ -27,27 +29,42 @@ module.exports = {
                     amount: functions.chest[x].amount
                 })
             }
-        }
-
-        // Checks if the player already has the item first
-        for (let x = 0; x < unlockingPlayer.inv.length; x++) {
-            for (let y = 0; y < functions.chest.length; y++) {
-                if (unlockingPlayer.inv[x].item == functions.chest[y].item) {
-                    unlockingPlayer.inv[x].amount += functions.chest[y].amount;
+        } else {
+            // Checks if the player already has the item first
+            for (let x = 0; x < functions.chest.length; x++) {
+                if (unlockingPlayer.inv.some(i => i.item == functions.chest[x].item)) {
+                    for (let y = 0; y < unlockingPlayer.inv.length; y++) {
+                        if (functions.chest[x].item == unlockingPlayer.inv[y].item) {
+                            unlockingPlayer.inv[y].amount += functions.chest[x].amount;
+                        }
+                    }
                 } else {
                     unlockingPlayer.inv.push({
-                    item: functions.chest[y].item,
-                    amount: functions.chest[y].amount
+                        item: functions.chest[x].item,
+                        amount: functions.chest[x].amount
                     });
                 }
             }
         }
-        functions.chest.length = 0;
+
+        
 
         fs.writeFile('./players.json', JSON.stringify(players), err => {
             if (err) console.error(err);
         });
 
-        message.channel.send(`${message.author.username} opened the chest!`)
+        let chestMessage = '';
+        for (let x = 0; x < functions.chest.length; x++) {
+            chestMessage += `${functions.chest[x].item} X${functions.chest[x].amount}\n`
+        }
+
+        const embed = new Discord.MessageEmbed()
+        .setColor(colors.rpgbot)
+        .setTitle(`${message.author.username} opened the chest!`)
+        .addField('You found:\n\n', chestMessage)
+
+        functions.chest.length = 0;
+
+        message.channel.send(embed);
     }
 }
