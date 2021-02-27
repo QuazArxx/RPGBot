e812e6767c82a2ead2fae336e8a5cd8fee2ea575
QuazArxx <33949974+QuazArxx@users.noreply.github.com>
2021-02-26 20:06:05 -0500

    Moved commands into directories

--- commands/show.js
+++
@@ -1,9 +0,0 @@
-const functions = require('../functions.js');
-
-module.exports = {
-    name: 'show',
-    description: 'Shows the chest contents',
-    execute(message, args) {
-        message.channel.send(require('util').inspect(functions.chest));
-    }
-}
