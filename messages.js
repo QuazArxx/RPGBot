b8e00a164e01af34d16b309a06c897b34b111481
QuazArxx <33949974+QuazArxx@users.noreply.github.com>
2021-02-28 23:22:45 -0500

    Fixed startAdventure and added noPermission

--- messages.js
+++ messages.js
@@ -5,13 +5,19 @@ const colors = require('./colors.json');
 module.exports = {
     startAdventure: new Discord.MessageEmbed()
     .setColor(colors.error)
-    .setTitle('You haven\'t started the adventure yet! Please type "r!start" to be able to battle.')
+    .setTitle('You haven\'t started the adventure yet! Please type "r!start" to begin!')
     
     ,
     
     chestSpawn: new Discord.MessageEmbed()
     .setColor(colors.chest)
     .setTitle('A chest spawned! Type "r!open" to open it!')
+
+    ,
+
+    chestDisappeared: new Discord.MessageEmbed()
+    .setColor(colors.error)
+    .setTitle('The chest disappeared!')
     
     ,
     
@@ -24,4 +30,10 @@ module.exports = {
     notInParty: new Discord.MessageEmbed()
     .setColor(colors.error)
     .setTitle('You\'re not part of the battle')
+
+    ,
+
+    noPermission: new Discord.MessageEmbed()
+    .setColor(colors.error)
+    .setTitle('You don\'t have permission to do that!')
 }
