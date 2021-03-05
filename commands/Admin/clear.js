775710b48e54f212e48806554a1de723ce1a99ea
QuazArxx <33949974+QuazArxx@users.noreply.github.com>
2021-03-05 14:30:35 -0500

    Added clearing mentioned player's inventory

--- commands/Admin/clear.js
+++ commands/Admin/clear.js
@@ -1,9 +1,11 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 
+const { prefix } = require('../../config.json');
 const functions = require('../../functions.js');
 const messages = require('../../messages.js');
 const colors = require('../../colors.json');
+const players = require('../../players.json');
 
 module.exports = {
     name: 'clear',
@@ -20,10 +22,10 @@ module.exports = {
             return message.channel.send(embed);
         } 
         
-        if (!(args[1] == 'chest' || args[1] == 'party' || args[1] == 'enemy')) {
+        if (!(args[1] == 'chest' || args[1] == 'party' || args[1] == 'enemy' || args[1] == "inv")) {
             const embed = new Discord.MessageEmbed()
             .setColor(colors.error)
-            .setTitle('Please choose "chest", "party", or "enemy" to clear.')
+            .setTitle('Please choose "chest", "party", "enemy", or "inv" to clear.')
 
             return message.channel.send(embed);
         }
@@ -40,6 +42,33 @@ module.exports = {
             functions.enemy.length = 0;
             message.delete();
             return message.channel.send('Enemy cleared successfully.')
+        } else if (args[1] == 'inv') {
+            if (!message.mentions.users.first()) {
+                const embed = new Discord.MessageEmbed()
+                .setColor(colors.error)
+                .setTitle('Please mention a user whose inventory you want cleared.')
+
+                return message.channel.send(embed);
+            } else if (!(players.some(user => user.id == message.mentions.users.first().id))) {
+                const embed1 = new Discord.MessageEmbed()
+                .setColor(colors.error)
+                .setTitle('Mentioned user isn\'t a player yet.')
+
+                return message.channel.send(embed1)
+            }
+
+            for (let x = 0; x < players.length; x++) {
+                if (players[x].id == message.mentions.users.first().id) {
+                    players[x].inv.length = 0;
+
+                    fs.writeFile('./players.json', JSON.stringify(players), err => {
+                        if (err) console.error(err);
+                    });
+                    
+                    message.delete();
+                    return message.channel.send(`${message.mentions.users.first().username}\'s inventory has been cleared successfully.`);
+                }
+            }
         }
     }
 }
